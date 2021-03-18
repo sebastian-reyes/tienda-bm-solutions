311a17673b939cc7f160528d868b2fe818474448
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-03-18 18:02:58 -0500

    Agregando productos a Hero

--- src/Components/Home/Hero.jsx
+++ src/Components/Home/Hero.jsx
@@ -1,28 +1,79 @@
 import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 import cam from '../../assets/img/hero/Logitech-C925E-01.png'
+import teclado from '../../assets/img/hero/teclado_hyperx_alloy_fps.png'
+import mouse from '../../assets/img/hero/razer_viper_ultimate.png'
 
 const Hero = () => {
     return (
         <Fragment>
             <section className="hero p-4 mb-5">
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="hero_producto">
-                                <div className="hero_producto-fondo"></div>
-                                <img src={cam} alt="producto_1" className="hero_producto-img img-fluid" />
+                    <div id="carousel-hero" className="carousel slide" data-bs-ride="carousel">
+                        <div className="carousel-inner carousel-hero">
+                            <div className="carousel-item active">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="hero_producto">
+                                            <div className="hero_producto-fondo"></div>
+                                            <img src={cam} alt="producto_1" className="hero_producto-img img-fluid" />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="hero_datos mt-5">
+                                            <span className="hero_datos-nuevo">Nuevo</span>
+                                            <h2 className="hero_datos-nombre titulo-principal mb-2">Cámara Logitech C925E</h2>
+                                            <p className="hero_datos-descripcion">Cámara web Logitech apta para cualquier tipo de trabajo.</p>
+                                            <Link to="/" className="btn btn-sesion">Cómprala ahora</Link>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-md-6">
-                            <div className="hero_datos mt-5">
-                                <span className="hero_datos-nuevo">Nuevo</span>
-                                <h2 className="hero_datos-nombre titulo-principal mb-2">Cámara Logitech C925E</h2>
-                                <p className="hero_datos-descripcion">Cámara web Logitech apta para cualquier tipo de trabajo.</p>
-                                <Link to="/" className="btn btn-sesion">Cómprala ahora</Link>
+                            <div className="carousel-item">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="hero_producto">
+                                            <div className="hero_producto-fondo"></div>
+                                            <img src={teclado} alt="producto_2" className="hero_producto-img img-fluid" />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="hero_datos mt-5">
+                                            <span className="hero_datos-nuevo">Nuevo</span>
+                                            <h2 className="hero_datos-nombre titulo-principal mb-2">Teclado HyperX Alloy FPS</h2>
+                                            <p className="hero_datos-descripcion">Teclado mecánico para gamers, disponible con 3 tipos de swtich</p>
+                                            <Link to="/" className="btn btn-sesion">Cómpralo ahora</Link>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
+                            <div className="carousel-item">
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="hero_producto">
+                                            <div className="hero_producto-fondo"></div>
+                                            <img src={mouse} alt="producto_3" className="hero_producto-img img-fluid" />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <div className="hero_datos mt-5">
+                                            <span className="hero_datos-nuevo">Nuevo</span>
+                                            <h2 className="hero_datos-nombre titulo-principal mb-2">Mouse Razer Viper Ultimate</h2>
+                                            <p className="hero_datos-descripcion">Mouse gamer wireless para unas buenas partidas.</p>
+                                            <Link to="/" className="btn btn-sesion">Cómpralo ahora</Link>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button className="carousel-control-prev" type="button" data-bs-target="#carousel-hero" data-bs-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Previous</span>
+                            </button>
+                            <button className="carousel-control-next" type="button" data-bs-target="#carousel-hero" data-bs-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="visually-hidden">Next</span>
+                            </button>
                         </div>
-
                     </div>
                 </div>
             </section>
